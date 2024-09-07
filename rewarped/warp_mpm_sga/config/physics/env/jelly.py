f1f8c7b2ebe756f2d8f0937e47d1957cf41855e8
etaoxing <etaoxing@users.noreply.github.com>
2024-09-07 17:48:25 -0400

    Change jelly cfg to use zerovel

--- rewarped/warp_mpm_sga/config/physics/env/jelly.py
+++ rewarped/warp_mpm_sga/config/physics/env/jelly.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass, field
 from .base import BaseEnvConfig
 from .physics import CorotatedPhysicsConfig
 from .shape import CubeShapeConfig
-from .vel import MildVelConfig
+from .vel import ZeroVelConfig
 
 @dataclass(kw_only=True)
 class JellyEnvConfig(BaseEnvConfig, name='jelly'):
@@ -11,4 +11,4 @@ class JellyEnvConfig(BaseEnvConfig, name='jelly'):
 
     physics: CorotatedPhysicsConfig = field(default_factory=CorotatedPhysicsConfig)
     shape: CubeShapeConfig = field(default_factory=CubeShapeConfig)
-    vel: MildVelConfig = field(default_factory=MildVelConfig)
+    vel: ZeroVelConfig = field(default_factory=ZeroVelConfig)
