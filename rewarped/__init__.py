8a72ebeb77f53d00aa701ecd7e9f2b76889fb629
etaoxing <etaoxing@users.noreply.github.com>
2025-02-28 05:39:17 -0500

    Pull in FeatherstoneIntegrator and override

--- rewarped/__init__.py
+++ rewarped/__init__.py
@@ -5,9 +5,9 @@ from .warp.import_mjcf import parse_mjcf
 
 wp.sim.parse_mjcf = parse_mjcf
 
-# from .warp.integrator_featherstone import FeatherstoneIntegrator
-# 
-# wp.sim.FeatherstoneIntegrator = FeatherstoneIntegrator
+from .warp.integrator_featherstone import FeatherstoneIntegrator
+
+wp.sim.FeatherstoneIntegrator = FeatherstoneIntegrator
 
 from .warp.integrator_mpm import MPMIntegrator
 
